69cfbb68e2e2b511d0b50e8b25f1be1ff50d38bc
Max Clayton Clowes <contact@mcclowes.com>
2022-08-02 23:31:14 +0100

    [feat] function created, output generated

--- normalize-data.py
+++ normalize-data.py
@@ -1,16 +1,74 @@
+import numpy
 import csv
+import functools
 
-rawData=[]
+def cleanF(n):
+  return float(n.strip().replace(",", ""))
 
-with open("csvs/hsi-price-2021-09.csv") as f:
-  for l in csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True):
-    rawData.append(l)
+def readFile(url):
+  rawData=[]
 
-print(rawData)
+  with open(url) as f:
+    for l in csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True):
+      rawData.append(l)
 
-# for date in data:
+  return rawData
 
+def writeFile(url, outputFile):
+  with open(url, 'w', encoding='UTF8') as outputFile:
+    writer = csv.writer(outputFile)
+    
 
-foo = numpy.unique(rawData, 0)
+    writer.writerow(['date', 'high', 'low'])
+    for point in ouputData:
+      writer.writerow(point)
 
-print(foo)
+def getDates(arr):
+  def flatten(x):
+      return x[0]
+
+  allDates=list(map(flatten, rawData))
+
+  return numpy.unique(allDates)
+
+def getHighsAndLows(date):
+  def pickMaxHigh(acc, dataPoint):
+    val=cleanF(dataPoint[7])
+
+    if dataPoint[0] != date:
+      return acc
+
+    if val > acc:
+      return float(val)
+    else:
+      return acc
+
+  def pickMinLow(acc, dataPoint):
+    val=cleanF(dataPoint[6])
+    
+    if dataPoint[0] != date:
+      return acc
+
+    if val < acc:
+      return float(val)
+    else:
+      return acc
+
+  maxHigh=functools.reduce(pickMaxHigh, rawData, 0)
+  minLow=functools.reduce(pickMinLow, rawData, 999999999999999)
+
+  return [date, maxHigh, minLow]
+
+files=[
+  "hsi-price-2021-09.csv",
+  "hsi-price-2021-10.csv",
+  "hsi-price-2021-11.csv",
+  "hsi-price-2021-12.csv",
+  "hsi-price-2022-01.csv",
+  "hsi-price-2022-02.csv",
+]
+for file in files:  
+  rawData = readFile("csvs/"+file)
+  dates = getDates(rawData)
+  ouputData=list(map(getHighsAndLows, dates))
+  writeFile('output/'+file, ouputData)
